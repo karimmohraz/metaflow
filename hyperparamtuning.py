616feee71dca4aaa108e007f2bd040f77974224b
karimmohraz <37623804+karimmohraz@users.noreply.github.com>
2021-06-10 11:14:55 +0200

    Add files via upload

--- hyperparamtuning.py
+++ hyperparamtuning.py
@@ -1,116 +1,134 @@
-from metaflow import FlowSpec, current, step, argo_base
+from metaflow import FlowSpec, current, step, argo_base, argo
 
 
-@argo_base(image='demisto/sklearn:1.0.0.16411',
+@argo_base(image='mlf.docker.repositories.sapcdn.io/aif/metaflow-sklearn:0.0.1',
            env=[{'name': 'AWS_ACCESS_KEY_ID', 'value': 'AKIAT4WR4G4TEMEBCPU6'},
-                {'name': 'AWS_SECRET_ACCESS_KEY', 'value': '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'}]
+                {'name': 'AWS_SECRET_ACCESS_KEY', 'value': '3H4WwRp07Gj493LecqV2RIs806zhpkY5apjm9s/B'}],
+           # tags required for AIF
+           annotations={'scenarios.ai.sap.com/name': 'metaflow-demo', "executables.ai.sap.com/name": "hyperhyper"},
+           labels={'scenarios.ai.sap.com/id': 'churn-prediction-test', 'ai.sap.com/version': '0.0.1'}, 
+           imagePullSecrets=[{'name': 'docker-registry-secret'}]
            )
 class HyperParamTuning(FlowSpec):
-    NUM_FOLDS = 4  # k-fold cross validation
+  """
+  Parallel training of MultiLayerPerceptron and RandomForest using hyperparameter search.
+  Select model with best performance.
+  """
 
-    @step
-    def start(self):
-        """
-        download iris data
-        split into train test
-        """
-        from sklearn import datasets
-        from sklearn.model_selection import train_test_split
+  NUM_FOLDS = 4  # k-fold cross validation
 
-        iris_df = datasets.load_iris(as_frame=True)['frame']
-        train, test = train_test_split(iris_df, test_size=0.4, stratify=iris_df['target'], random_state=42)
-        self.X_train = train[train.columns[0:4]].to_numpy()
-        self.y_train = train.target.to_numpy()
-        self.X_test = test[train.columns[0:4]].to_numpy()
-        self.y_test = test.target.to_numpy()
+  @step
+  def start(self):
+    """
+    download iris data
+    split into train test
+    """
+    from sklearn import datasets
+    from sklearn.model_selection import train_test_split
 
-        self.next(self.multi_layer_perceptron, self.random_forest)
+    iris_df = datasets.load_iris(as_frame=True)['frame']
+    train, test = train_test_split(iris_df, test_size=0.4, stratify=iris_df['target'], random_state=42)
+    self.X_train = train[train.columns[0:4]].to_numpy()
+    self.y_train = train.target.to_numpy()
+    self.X_test = test[train.columns[0:4]].to_numpy()
+    self.y_test = test.target.to_numpy()
 
-    @step
-    def multi_layer_perceptron(self):
-        """
-        train multi-layer-perceptron and fine-tune parameters
-        """
-        from sklearn.neural_network import MLPClassifier
+    self.next(self.multilayer_perceptron, self.random_forest)
 
-        self.mlp = MLPClassifier(alpha=1, max_iter=10000, random_state=42)
+  @step
+  def multilayer_perceptron(self):
+    from sklearn.neural_network import MLPClassifier
 
-        self.next(self.gridsearch_cv)
+    self.mlp = MLPClassifier(alpha=1, max_iter=3000, random_state=42)
 
-    @step
-    def gridsearch_cv(self):
-        """
-        full hyperparameter search
-        """
-        from sklearn.model_selection import GridSearchCV
+    self.next(self.gridsearch_cv)
 
-        parameter_space = {
-            'activation': ['tanh', 'relu'],
-            'solver': ['sgd', 'adam'],
-            'learning_rate': ['constant', 'adaptive'],
-        }
+  @step
+  def gridsearch_cv(self):
+    """
+    full hyperparameter search
+    """
+    from sklearn.model_selection import GridSearchCV
 
-        self.clf = GridSearchCV(self.mlp, parameter_space, cv=4, n_jobs=-1)
-        self.clf.fit(self.X_train, self.y_train) 
+    parameter_space = {
+        'activation': ['tanh', 'relu'],
+        'solver': ['sgd', 'adam'],
+        'learning_rate': ['constant', 'adaptive'],
+    }
 
-        self.next(self.join)
+    self.clf = GridSearchCV(self.mlp, parameter_space, cv=HyperParamTuning.NUM_FOLDS, n_jobs=-1)
+    self.clf.fit(self.X_train, self.y_train) 
 
-    @step
-    def random_forest(self):
-      """
-      Random Forest classifier
-      """
-      from sklearn.ensemble import RandomForestClassifier
+    self.next(self.compare_models)
 
-      self.rf = RandomForestClassifier(random_state=42)
+  @step
+  def random_forest(self):
+    """
+    Random Forest classifier
+    """
+    from sklearn.ensemble import RandomForestClassifier
 
-      self.next(self.randomizedsearch_cv)
+    self.rf = RandomForestClassifier(random_state=42)
 
-    @step
-    def randomizedsearch_cv(self):
-      """
-      randomized search due to big parameter space for random forest
-      """
-      from sklearn.model_selection import RandomizedSearchCV
+    self.next(self.randomizedsearch_cv)
 
-      random_grid = {
-        'n_estimators': [20, 50, 100, 150, 200, 300],
-        'max_features': ['auto', 0.7],
-        'max_depth': [5, 10, 30, 50, 75, 100]
-      }
-      
-      self.clf = RandomizedSearchCV(self.rf, random_grid, n_iter=8, cv=4, n_jobs=-1, random_state=42)
-      self.clf.fit(self.X_train, self.y_train)
+  @step
+  def randomizedsearch_cv(self):
+    """
+    randomized search due to big parameter space for random forest
+    """
+    from sklearn.model_selection import RandomizedSearchCV
 
-      self.next(self.join)
+    random_grid = {
+      'n_estimators': [20, 30, 50, 70, 100],
+      'max_features': ['auto', 0.7],
+      'max_depth': [5, 10, 30, 50, 80]
+    }
+    
+    self.clf = RandomizedSearchCV(self.rf, random_grid, n_iter=8, cv=HyperParamTuning.NUM_FOLDS, n_jobs=-1, random_state=42)
+    self.clf.fit(self.X_train, self.y_train)
 
-    @step
-    def join(self, inputs):
-      self.clfs = [inp.clf for inp in inputs]
-      self.merge_artifacts(inputs, exclude=['clf'])
+    self.next(self.compare_models)
 
-      self.next(self.end)
+  @step
+  def compare_models(self, inputs):
+    """
+    join parallel training results
+    """
+    self.clfs = [inp.clf for inp in inputs]
+    self.merge_artifacts(inputs, exclude=['clf'])
 
-    @step
-    def end(self):
-        """
-        compare scores from single step to parallel calculation
-        """
-        from sklearn import metrics
-
-        for clf in self.clfs:
-          print('Best parameters found:\n', clf.best_params_)
-          means = clf.cv_results_['mean_test_score']
-          stds = clf.cv_results_['std_test_score']
-          for mean, std, params in zip(means, stds, clf.cv_results_['params']):
-              print("%0.3f (+/-%0.03f) for %r" % (mean, std * 2, params))
-
-          # Evaluate on test set
-          y_true, y_pred = self.y_test , clf.predict(self.X_test)
-          from sklearn.metrics import classification_report
-          print('Results on the test set:')
-          print(classification_report(y_true, y_pred))    
+    for clf in self.clfs:
+      print('Best parameters found:\n', clf.best_params_)
+      means = clf.cv_results_['mean_test_score']
+      stds = clf.cv_results_['std_test_score']
+      for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+          print("%0.3f (+/-%0.03f) for %r" % (mean, std * 2, params))
           
+    self.next(self.end)
+
+  @step
+  def end(self):
+    """
+    evaluate best models on test set
+    """
+    from sklearn.metrics import classification_report
+    from sklearn.metrics import accuracy_score
+
+    best_score = 0
+    best_clf = ''
+    for clf in self.clfs:
+      # Evaluate on test set
+      y_true, y_pred = self.y_test , clf.predict(self.X_test)
+      print('Results on the test set for ', clf.estimator.__class__.__name__)
+      print(classification_report(y_true, y_pred, target_names=['Setosa     ', 'Versicolour', 'Virginica  ']))    
+      if accuracy_score(y_true, y_pred) > best_score:
+        best_clf = clf.estimator.__class__.__name__
+        best_score = accuracy_score(y_true, y_pred)
+        
+    # save best model
+    print('\nBest model: {} with test accuracy: {}'.format(best_clf, best_score))
+
 
 if __name__ == '__main__':
     HyperParamTuning()
