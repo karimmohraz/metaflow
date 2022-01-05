dc23cc6250be227cdc7ec93246846ac3197f7747
karimmohraz <37623804+karimmohraz@users.noreply.github.com>
2022-01-05 13:06:25 +0100

    Update hyperparamtuning.py

--- hyperparamtuning.py
+++ hyperparamtuning.py
@@ -2,8 +2,8 @@ from metaflow import FlowSpec, current, step, argo_base, argo
 
 
 @argo_base(image='mlf.docker.repositories.sapcdn.io/aif/metaflow-sklearn:0.0.1',
-#            env=[{'name': 'AWS_ACCESS_KEY_ID', 'value': 'AKIAT4WR4G4TEMEBCPU6'},
-#                 {'name': 'AWS_SECRET_ACCESS_KEY', 'value': '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'}],
+#            env=[{'name': 'AWS_ACCESS_KEY_ID', 'value': 'xxxx'},
+#                 {'name': 'AWS_SECRET_ACCESS_KEY', 'value': 'xxxx/B'}],
            envFrom=[{'secretRef': {'name': 'default-object-store-secret'}}],
            imagePullSecrets=[{'name': 'docker-registry-secret'}],
            annotations={'scenarios.ai.sap.com/name': 'metaflow-demo', 'executables.ai.sap.com/name': 'hyperparamtuning'},
